10460ab112c1ff90576376c03fa9b02902a8c621
Raul Solano <rasoberdiel3@gmail.com>
2024-03-19 13:09:21 +0100

    mapper done

--- cinemapedia/lib/config/router/app_router.dart
+++ cinemapedia/lib/config/router/app_router.dart
@@ -1,10 +1,13 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-    path: '/',
-    name: HomeScreen.name,
-    builder: (context, state) => const HomeScreen(),
-  )
-]);
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+    )
+  ],
+);
