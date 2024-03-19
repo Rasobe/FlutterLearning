2bc5f8873765ead56e807b4281455363b5db3722
Raul Solano <rasoberdiel3@gmail.com>
2024-03-19 12:17:00 +0100

    Environment vairable configured

--- cinemapedia/lib/main.dart
+++ cinemapedia/lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(const MyApp());
+import 'package:cinemapedia/config/theme/app_theme.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
